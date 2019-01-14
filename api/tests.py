5d6d27b3065650e0008118c6b9c4e513dff0724d
Felipe Ferreira <felipe.gomes.ferreira@gmail.com>
2019-01-14 16:49:40 -0200

    adição do método de gravação no hdfs

--- api/tests.py
+++ api/tests.py
@@ -5,6 +5,7 @@ from django.test import RequestFactory, TestCase
 from django.urls import reverse
 from hashlib import md5
 from io import BytesIO
+from unittest import mock
 from .utils import securedecorator, md5reader
 
 # Create your tests here.
@@ -65,7 +66,8 @@ class TestMd5Reader(TestCase):
         )
 
 class TestUpload(TestCase):
-    def test_file_post(self):
+    @mock.patch('api.views.upload_to_hdfs')
+    def test_file_post(self, upload_to_hdfs):
         contents = b'filecontents'
 
         contents_md5 = md5(contents).hexdigest()
@@ -78,14 +80,17 @@ class TestUpload(TestCase):
             {
                 'SECRET': settings.SECRET,
                 'md5': contents_md5,
+                'method': 'cpf',
                 'file': contents_file
             }
         )
 
         self.assertEquals(response.status_code, 201)
         self.assertEquals(response.json()['md5'], contents_md5)
+        upload_to_hdfs.assert_called_once()
 
-    def test_file_post_wrong_md5(self):
+    @mock.patch('api.views.upload_to_hdfs')
+    def test_file_post_wrong_md5(self, upload_to_hdfs):
         contents = b'filecontents'
 
         contents_md5 = md5(contents).hexdigest()
@@ -98,9 +103,11 @@ class TestUpload(TestCase):
             {
                 'SECRET': settings.SECRET,
                 'md5': 'wrongmd5',
+                'method': 'cpf',
                 'file': contents_file
             }
         )
 
         self.assertEquals(response.status_code, 500)
         self.assertEquals(response.json()['md5'], contents_md5)
+        upload_to_hdfs.assert_not_called()
--- api/views.py
+++ api/views.py
@@ -1,7 +1,9 @@
 from django.http import JsonResponse
 from django.shortcuts import render
-from .clients import hdfsclient
+from os import path
+from .clients import hdfsclient, methodmap
 from .utils import md5reader
+
 
 
 BASE_RETURN = {
@@ -9,12 +11,26 @@ BASE_RETURN = {
 }
 
 
+def upload_to_hdfs(file, filename, destination):
+    hdfsclient.write(
+        path.join(destination, filename),
+        file
+    )
+
+
 def upload(request):
     file = request.FILES['file']
+    method = request.POST.get('method')
+    filename = request.POST.get('filename')
+
     BASE_RETURN['md5'] = md5reader(file)
 
     if BASE_RETURN['md5'] != request.POST.get('md5'):
         return JsonResponse(BASE_RETURN, status=500)
+
+    destination = methodmap[method]
+
+    upload_to_hdfs(file.file, filename, destination)
 
     return JsonResponse(
         BASE_RETURN,
