decdfc5d819c1dcaf8a2945e87e0de3b5836daef
Rhenan Bartels <rhenan.bartels@gmail.com>
2019-03-12 17:23:20 -0300

    Retorna form no template

--- secret/views.py
+++ secret/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from secret.forms import SecretForm
 
@@ -7,4 +8,7 @@ def create_secret(request):
     form = SecretForm(request.POST or None)
     if form.is_valid():
         form.save()
-    return HttpResponse()
+        return HttpResponse()
+
+    context = {'form': form}
+    return render(request, 'secret/create-secret.html', context)
