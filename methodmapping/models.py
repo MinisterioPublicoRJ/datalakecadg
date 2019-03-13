9e6d8cb5a01a06ffc097b98094df081683433f90
Rhenan Bartels <rhenan.bartels@gmail.com>
2019-03-13 11:54:44 -0300

    Cria representacao texto para Secret

--- methodmapping/models.py
+++ methodmapping/models.py
@@ -7,3 +7,6 @@ class MethodMapping(models.Model):
     method = models.CharField(max_length=255)
     uri = models.CharField(max_length=255)
     secrets = models.ManyToManyField(Secret)
+
+    def __str__(self):
+        return f'{self.method}: {self.uri}'
