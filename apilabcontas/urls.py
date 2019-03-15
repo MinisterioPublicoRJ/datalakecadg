14a25b5e6f236318e0fac2ce89f61d29c0509661
Rhenan Bartels <rhenan.bartels@gmail.com>
2019-03-15 11:55:47 -0300

    Adiciona url para arquivos estáticos

--- apilabcontas/urls.py
+++ apilabcontas/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
@@ -23,4 +25,7 @@ urlpatterns = [
     path('secret/', include('secret.urls')),
     path('accounts/login/', LoginView.as_view(), name='login'),
     path('accounts/logout/', LogoutView.as_view(), name='logout'),
-]
+] + static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT
+)
