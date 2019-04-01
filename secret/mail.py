19a1bdffab7097dfe25df5ed0119a4b697ce2047
Rhenan Bartels <rhenan.bartels@gmail.com>
2019-04-01 17:31:44 -0300

    Add encoding to email msg

--- secret/mail.py
+++ secret/mail.py
@@ -1,5 +1,9 @@
 import smtplib
 
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.header import Header
+
 from decouple import config
 
 
@@ -9,10 +13,17 @@ def login():
 
 
 def send_mail(server, msg, dest):
+    msg_mime = MIMEMultipart('alternative')
+    msg_mime.set_charset('utf8')
+    msg_mime['FROM'] = config('EMAIL_HOST_USER')
+    msg_mime['Subject'] = config('EMAIL_SUBJECT')
+    msg_mime['To']
+    attach = MIMEText(msg.encode('utf-8'), 'html', 'UTF-8')
+    msg_mime.attach(attach)
     server.sendmail(
         config('EMAIL_HOST_USER'),
         dest,
-        msg
+        msg_mime.as_string()
     )
 
 
