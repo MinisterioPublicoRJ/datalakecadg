70fd01af7ee0a8378a8fb2a1ef7b4b8bb880cbd6
Rhenan Bartels <rhenan.bartels@gmail.com>
2019-05-07 11:50:44 -0300

    Simplify is_header_valid function

--- api/tests/test_utils.py
+++ api/tests/test_utils.py
@@ -1,11 +1,14 @@
+import gzip
+
 from hashlib import md5
+from unittest import mock
 
 from django.core.exceptions import PermissionDenied
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import RequestFactory, TestCase
 from model_mommy.mommy import make
 
-from api.utils import securedecorator, md5reader
+from api.utils import securedecorator, md5reader, is_header_valid
 
 
 @securedecorator
@@ -100,3 +103,22 @@ class TestMd5Reader(TestCase):
             md5reader(uploadedfile),
             md5(b'lerolero').hexdigest()
         )
+
+
+class TestValidHeader(TestCase):
+    @mock.patch('secret.models.send_mail')
+    def test_validate_file_header(self, _send_mail):
+        gzipped_file = open('api/tests/csv_example.csv.gz', 'rb')
+        secret = make('secret.Secret', username='anyname')
+        mmap = make(
+            'methodmapping.MethodMapping',
+            method='cpf',
+            uri='/path/to/storage/cpf',
+            mandatory_headers='field1,field2,field3'
+        )
+        secret.methods.add(mmap)
+
+        valid, status = is_header_valid(secret.username, 'cpf', gzipped_file)
+        import ipdb;ipdb.set_trace()
+
+        self.assertTrue(valid)
