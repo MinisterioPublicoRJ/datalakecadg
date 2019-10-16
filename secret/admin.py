98737ff1416ac14bb129507ad6f78a690d78d715
Rhenan Bartels <rhenan.bartels@gmail.com>
2019-10-16 18:44:26 -0300

    Sort methods by uri in admin

--- secret/admin.py
+++ secret/admin.py
@@ -1,11 +1,25 @@
+from django import forms
+from django.db.models.functions import Lower
 from django.contrib import admin
 
+from methodmapping.models import MethodMapping
 from secret.models import Secret
+
+
+class SecretAdminForm(forms.ModelForm):
+    methods = forms.ModelMultipleChoiceField(
+        queryset=MethodMapping.objects.all().order_by(Lower('uri'))
+    )
+
+    class Meta:
+        model = MethodMapping
+        fields = '__all__'
 
 
 class SecretAdmin(admin.ModelAdmin):
     readonly_fields = ['secret_key']
     list_display = ('username', 'email', 'secret_key')
+    form = SecretAdminForm
 
 
 admin.site.register(Secret, SecretAdmin)
