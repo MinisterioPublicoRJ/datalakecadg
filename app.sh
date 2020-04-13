649a7ea500eeea8dc31e4ddc65d1b87ec85fc061
rhenanbartels <rhenan.bartels@gmail.com>
2020-04-13 17:21:12 -0300

    Atualiza workers do gunicorn

--- app.sh
+++ app.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gunicorn datalakecadg.wsgi:application --bind=0.0.0.0:8080 -t 180 --log-file -
+gunicorn datalakecadg.wsgi:application --workers=12 --threads=2 --bind=0.0.0.0:8080 -t 180 --log-file -
