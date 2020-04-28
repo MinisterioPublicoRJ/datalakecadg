b39faf1623a7ac3889cee86fd8691d4908bd72e6
rhenanbartels <rhenan.bartels@gmail.com>
2020-04-28 09:28:55 -0300

    Cria função para ler cabeçalho

--- api/utils.py
+++ api/utils.py
@@ -37,6 +37,17 @@ def md5reader(uploadedfile):
     return hash_md5.hexdigest()
 
 
+def get_file_header(file_):
+    with gzip.open(file_, 'rt', newline='', encoding='utf-8-sig') as fobj:
+        dialect = csv.Sniffer().sniff(fobj.readline())
+        fobj.seek(0)
+        reader = csv.reader(fobj, dialect)
+        header = next(reader)
+        file_.seek(0)
+
+    return header, file_
+
+
 def is_header_valid(username, method, file_):
     dest = Secret.objects.filter(
         username=username,
@@ -48,21 +59,16 @@ def is_header_valid(username, method, file_):
         if expected_headers == '':
             return True, {}
 
-        with gzip.open(file_, 'rt', newline='', encoding='utf-8-sig') as fobj:
-            dialect = csv.Sniffer().sniff(fobj.readline())
-            fobj.seek(0)
-            reader = csv.reader(fobj, dialect)
-            header = next(reader)
-            file_.seek(0)
-            if header == expected_headers.split(','):
-                return True, {}
-            else:
-                return (False,
-                        'File must contain the following headers: {0}.'
-                        ' Received: {1}'.format(
-                            expected_headers,
-                            ','.join(header)
-                        ))
+        header, file_ = get_file_header(file_)
+        if header == expected_headers.split(','):
+            return True, {}
+        else:
+            return (False,
+                    'File must contain the following headers: {0}.'
+                    ' Received: {1}'.format(
+                        expected_headers,
+                        ','.join(header)
+                    ))
 
 
 def get_destination(username, method):
