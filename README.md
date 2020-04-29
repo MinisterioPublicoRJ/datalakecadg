2285b8b52232bc9eb71d3f617639a322167ffa74
rhenanbartels <rhenan.bartels@gmail.com>
2020-04-29 17:02:46 -0300

    Atualiza README

--- README.md
+++ README.md
@@ -4,3 +4,18 @@ Serviço para recebimento de dados de parceiros do Estado do Rio de Janeiro
 pelo convênio firmado entre MPRJ e Governo do Estado do Rio de Janeiro.
 
 [https://datalakecadg.mprj.mp.br](https://datalakecadg.mprj.mp.br)
+
+# Executando testes de integração com HDFS
+
+```
+chmod +x integrations_tests.sh
+
+./integrations_tests.sh METHOD_NAME USERNAME SECRET_KEY HDFS_URI [FILEPATH, SCHEMA_PATH]
+```
+
+`FILEPATH` = caminho até o arquivo que será enviado ao HDFS com extensão .csv.gz
+
+`SCHEMA_PATH` = caminho até o arquivo contendo o schema com extensão .json
+
+**OBS**: Caso os argumentos `FILEPATH` e `SCHEMA_PATH` não forem fornecidos, o teste
+será executado com os arquivos que estão em `methodmapping/tests/fixtures/`
---
+++ methodmapping/tests/test_integration.py
@@ -0,0 +1,69 @@
+import json
+from datetime import date as dt
+from hashlib import md5
+from unittest import mock, skipIf
+
+from decouple import config
+from django.test import TestCase
+from django.urls import reverse
+from model_mommy.mommy import make
+from unipath import Path
+
+
+FIXTURES_DIR = Path(__file__).parent.child("fixtures")
+
+
+class DatabaseIntegrationTest(TestCase):
+    @skipIf(
+        config("SKIP_INTEGRATION_TESTS", True, cast=bool),
+        "Skipping integration test!"
+    )
+    @mock.patch("secret.models.login")
+    def test_hdfs_integrations_test(self, _login):
+        # TEST CONFIG
+        filepath = config(
+            "INTEGRATION_TEST_FILEPATH",
+            FIXTURES_DIR.child("integration_test_file.csv.gz")
+        )
+        method = config("INTEGRATION_TEST_METHOD_NAME")
+        username = config("INTEGRATION_TEST_USERNAME")
+        secret_key = config("INTEGRATION_TEST_SECRET_KEY")
+        hdfs_uri = config("INTEGRATION_TEST_HDFS_URI")
+        schema_path = config(
+            "INTEGRATION_TEST_SCHEMA_PATH",
+            FIXTURES_DIR.child("integration_test_schema.json")
+        )
+
+        with open(schema_path) as fobj:
+            schema = json.load(fobj)
+
+        secret_obj = make(
+            "secret.Secret",
+            username=username,
+            secret_key=secret_key
+        )
+        method_obj = make(
+            "methodmapping.MethodMapping",
+            method=method,
+            uri=hdfs_uri,
+            schema=schema
+        )
+        secret_obj.methods.add(method_obj)
+        secret_obj.save()
+
+        with open(filepath, mode="rb") as file_:
+            contents_md5 = md5(file_.read()).hexdigest()
+            file_.seek(0)
+            response = self.client.post(
+                reverse('api-upload'),
+                {
+                    'SECRET': secret_key,
+                    'nome': username,
+                    'md5': contents_md5,
+                    'method': method,
+                    'file': file_,
+                    'filename': f'integration_test_{dt.today()}.csv.gz'
+                }
+            )
+
+        self.assertEqual(response.status_code, 200)
