48a3d5a328af6588fe4bcf87235a81238095de5e
rhenanbartels <rhenan.bartels@gmail.com>
2020-05-06 11:52:29 -0300

    Corrige nome de campos e encerra a validação no primeiro erro

--- api/forms.py
+++ api/forms.py
@@ -1,14 +1,67 @@
 from django import forms
+from django.forms.utils import ErrorDict
 
 from api.utils import is_data_valid, md5reader
 
 
 class FileUploadForm(forms.Form):
-    username = forms.CharField(max_length=255)
+    nome = forms.CharField(max_length=255)
     method = forms.CharField(max_length=255)
     filename = forms.CharField(max_length=255)
-    sent_md5 = forms.CharField(max_length=32)
+    md5 = forms.CharField(max_length=32)
     file = forms.FileField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.md5_ = md5reader(self.files["file"])
+
+    # Os métodos _clean_fields e full_clean foram alterados para que a
+    # validação pare no primeiro erro
+    def _clean_fields(self):
+        for name, field in self.fields.items():
+            # value_from_datadict() gets the data from the data dictionaries.
+            # Each widget type knows how to retrieve its own data, because some
+            # widgets split data over several HTML fields.
+            if field.disabled:
+                value = self.get_initial_for_field(field, name)
+            else:
+                value = field.widget.value_from_datadict(self.data, self.files, self.add_prefix(name))
+            try:
+                if isinstance(field, forms.FileField):
+                    initial = self.get_initial_for_field(field, name)
+                    value = field.clean(value, initial)
+                else:
+                    value = field.clean(value)
+                self.cleaned_data[name] = value
+                if hasattr(self, 'clean_%s' % name):
+                    value = getattr(self, 'clean_%s' % name)()
+                    self.cleaned_data[name] = value
+            except forms.ValidationError as e:
+                self.add_error(name, e)
+                break
+
+    def full_clean(self):
+        """
+        Clean all of self.data and populate self._errors and self.cleaned_data.
+        """
+        self._errors = ErrorDict()
+        if not self.is_bound:  # Stop further processing.
+            return
+        self.cleaned_data = {}
+        # If the form is permitted to be empty, and none of the form data has
+        # changed from the initial data, short circuit any validation.
+        if self.empty_permitted and not self.has_changed():
+            return
+
+        clean_methods = [
+            self._clean_fields,
+            self._clean_form,
+            self._post_clean,
+        ]
+        for clean_method in clean_methods:
+            clean_method()
+            if self._errors:
+                break
 
     @property
     def base_return(self):
@@ -17,23 +70,24 @@ class FileUploadForm(forms.Form):
 
     @property
     def status_code(self):
-        error_types = [list(error.keys())[0] for error in self._errors]
-        status = 200
-        if "sent_md5" in error_types or "schema" in error_types:
+        error_types = [error for error in self._errors]
+        status = 400
+        if not self._errors:
+            status = 201
+        elif "md5" in error_types or "schema" in error_types:
             status = 400
         elif "filename" in error_types:
             status = 415
 
         return status
 
-    def clean_sent_md5(self):
-        sent_md5 = self.cleaned_data["sent_md5"]
+    def clean_md5(self):
+        md5 = self.cleaned_data["md5"]
         file_ = self.files["file"]
-        self.md5_ = md5reader(file_)
-        if sent_md5 != self.md5_:
+        if md5 != self.md5_:
             raise forms.ValidationError("valor md5 não confere!")
 
-        return sent_md5
+        return md5
 
     def clean_filename(self):
         filename = self.cleaned_data["filename"]
@@ -47,7 +101,7 @@ class FileUploadForm(forms.Form):
     def clean(self):
         cleaned_data = super().clean()
         valid_data, status = is_data_valid(
-            cleaned_data["username"],
+            cleaned_data["nome"],
             cleaned_data["method"],
             self.files["file"]
         )
--- api/tests/test_validator.py
+++ api/tests/test_validator.py
@@ -11,10 +11,10 @@ class TestValidator(TestCase):
     def test_happy_path(self, _md5reader, _is_data_valid):
         filename = "FILENAME.gz"
         data = {
-            "username": "USERNAME",
+            "nome": "USERNAME",
             "method": "METHOD-NAME",
             "filename": filename,
-            "sent_md5": "MD5",
+            "md5": "MD5",
         }
         file_to_send = {"file": SimpleUploadedFile(filename, b"content")}
         form = FileUploadForm(data=data, files=file_to_send)
@@ -25,13 +25,13 @@ class TestValidator(TestCase):
 
     @mock.patch("api.forms.is_data_valid", return_value=(True, {}))
     @mock.patch("api.forms.md5reader", return_value="md5 sum")
-    def test_invalid_sent_md5(self, _md5reader, _is_data_valid):
+    def test_invalid_md5(self, _md5reader, _is_data_valid):
         filename = "FILENAME.gz"
         data = {
-            "username": "USERNAME",
+            "nome": "USERNAME",
             "method": "METHOD-NAME",
             "filename": filename,
-            "sent_md5": "WRONG MD5",
+            "md5": "WRONG MD5",
         }
         file_to_send = {"file": SimpleUploadedFile(filename, b"content")}
         form = FileUploadForm(data=data, files=file_to_send)
@@ -39,17 +39,17 @@ class TestValidator(TestCase):
 
         self.assertFalse(is_valid)
         self.assertEqual(form.md5_, "md5 sum")
-        self.assertEqual(form.errors["sent_md5"], ["valor md5 não confere!"])
+        self.assertEqual(form.errors["md5"], ["valor md5 não confere!"])
 
     @mock.patch("api.forms.is_data_valid", return_value=(True, {}))
     @mock.patch("api.forms.md5reader", return_value="MD5")
     def test_invalid_file_extension(self, _md5reader, _is_data_valid):
         filename = "FILENAME"
         data = {
-            "username": "USERNAME",
+            "nome": "USERNAME",
             "method": "METHOD-NAME",
             "filename": filename,
-            "sent_md5": "WRONG MD5",
+            "md5": "WRONG MD5",
         }
         file_to_send = {"file": SimpleUploadedFile(filename, b"content")}
         form = FileUploadForm(data=data, files=file_to_send)
@@ -62,12 +62,12 @@ class TestValidator(TestCase):
     @mock.patch("api.forms.md5reader", return_value="MD5")
     def test_invalid_data_schema(self, _md5reader, _is_data_valid):
         _is_data_valid.return_value = (False, {"error": "error-msg"})
-        filename = "FILENAME"
+        filename = "FILENAME.gz"
         data = {
-            "username": "USERNAME",
+            "nome": "USERNAME",
             "method": "METHOD-NAME",
             "filename": filename,
-            "sent_md5": "WRONG MD5",
+            "md5": "MD5",
         }
         file_to_send = {"file": SimpleUploadedFile(filename, b"content")}
         form = FileUploadForm(data=data, files=file_to_send)
@@ -84,10 +84,10 @@ class TestValidator(TestCase):
     def test_create_base_return(self, _md5reader, _is_data_valid):
         filename = "FILENAME.gz"
         data = {
-            "username": "USERNAME",
+            "nome": "USERNAME",
             "method": "METHOD-NAME",
             "filename": filename,
-            "sent_md5": "WRONG MD5",
+            "md5": "WRONG MD5",
         }
         file_to_send = {"file": SimpleUploadedFile(filename, b"content")}
         form = FileUploadForm(data=data, files=file_to_send)
@@ -98,23 +98,40 @@ class TestValidator(TestCase):
         self.assertEqual(
             base_return,
             {
-                "error": {"sent_md5": ["valor md5 não confere!"]},
+                "error": {"md5": ["valor md5 não confere!"]},
                 "md5": 'md5 sum'
             }
         )
 
+    def test_form_stop_first_error(self):
+        "Os dados possuem dois erros. A vaidação deve parar no primeiro error"
+        filename = "FILENAME"
+        data = {
+            "nome": "USERNAME",
+            "method": "METHOD-NAME",
+            "filename": filename,
+            "md5": "WRONG MD5",
+        }
+        file_to_send = {"file": SimpleUploadedFile(filename, b"content")}
+        form = FileUploadForm(data=data, files=file_to_send)
+        is_valid = form.is_valid()
+
+        self.assertFalse(is_valid)
+        self.assertEqual(len(form.errors), 1)
+
+    @mock.patch("api.forms.md5reader")
     @mock.patch.object(FileUploadForm, "is_valid")
-    def test_create_status_code(self, _is_valid):
-        form_400_schema = FileUploadForm(data=dict(), files="file")
-        form_400_schema._errors = [{"schema": "schema invalido"}]
-        form_415 = FileUploadForm(data=dict(), files="file")
-        form_415._errors = [{"filename": "arquivo deve ser gzip"}]
-        form_400_md5 = FileUploadForm(data=dict(), files="file")
-        form_400_md5._errors = [{"sent_md5": "md5 não confere"}]
-        form_200 = FileUploadForm(data=dict(), files="file")
-        form_200._errors = []
+    def test_create_status_code(self, _is_valid, _md5reader):
+        form_400_schema = FileUploadForm(data=dict(), files={"file": b"file"})
+        form_400_schema._errors = {"schema": "schema invalido"}
+        form_415 = FileUploadForm(data=dict(), files={"file": b"file"})
+        form_415._errors = {"filename": "arquivo deve ser gzip"}
+        form_400_md5 = FileUploadForm(data=dict(), files={"file": b"file"})
+        form_400_md5._errors = {"md5": "md5 não confere"}
+        form_201 = FileUploadForm(data=dict(), files={"file": b"file"})
+        form_201._errors = []
 
         self.assertEqual(form_400_schema.status_code, 400)
         self.assertEqual(form_400_md5.status_code, 400)
         self.assertEqual(form_415.status_code, 415)
-        self.assertEqual(form_200.status_code, 200)
+        self.assertEqual(form_201.status_code, 201)
